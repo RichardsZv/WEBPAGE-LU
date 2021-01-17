6f75a6c0fa61cd5563a6679635b67eb931e6f317
Ričards <richards.zvagulis@gmail.com>
2021-01-17 18:44:39 +0200

    dom implementation

---
+++ js/sportJs.js
@@ -0,0 +1,14 @@
+var data = ["Kļūda etapā", "OK", "OK", "OK", "OK", "OK", "Kļūda ņemšanā", "Kļūda etapā", "OK", "Kļūda etapā", "Kļūda etapā + kļūda ņemšanā", "OK", "OK", "Kļūda etapā", "OK", "OK", "OK", "OK"];
+var i = 0;
+function function1() {
+    var ol = document.getElementById("list");
+    var li = document.createElement("li");
+    if(i<data.length){
+        li.appendChild(document.createTextNode(data[i]));
+        ol.appendChild(li);
+        i++;
+    }
+    else{
+        alert("Visi punkti analizeti:)");
+    }
+}
