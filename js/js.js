3ce21749de95b1167680c58579569de9abda063d
Ričards <richards.zvagulis@gmail.com>
2021-01-18 12:53:45 +0200

    landingAnimation.js creation

--- js/js.js
+++ js/js.js
@@ -22,12 +22,3 @@ navSlide();
 
 
 
-
-//LANDING PAGE ANIMATION
-$(function() {
-    $('.button').animate({ opacity:'0.7' }, 1000, 'linear');
-    $('.media-btns').animate({ opacity:'0.7' }, 1000, 'linear');
-});
-
-
-
---
+++ js/landingAnimation.js
@@ -0,0 +1,6 @@
+
+//LANDING PAGE ANIMATION
+$(function() {
+    $('.button').animate({ opacity:'0.7' }, 1000, 'linear');
+    $('.media-btns').animate({ opacity:'0.7' }, 1000, 'linear');
+});
